f2e7e424536bcbaf5bd46bb46c44351c66c84c5c
takkiiiiiiiii <yudai2002brazil@gmail.com>
2025-07-19 16:06:22 +0900

    WIP:QBER

--- libs/default_parameters.py
+++ libs/default_parameters.py
@@ -58,18 +58,18 @@
 
 # Yudai # Default parameters for the simulation
 a = 0.75 # Aperture radius of the receiver telescope (m)
-n_s = 0.3 # Mean photon number per pulse (for single photon source)
+n_s = 0.5 # Mean photon number per pulse (for single photon source)
 lambda_signal = 0.5 * n_s
 wavelength = 0.85e-6 # Wavelength of light (m)
 h_OGS = 10 # Height of Optical Ground Station (m)
-h_s = 550 * 1000 # Height of Satellite (m)
+h_s = 500 * 1000 # Height of Satellite (m)
 h_atm = 20 * 1000 # Height of atmosphere (m)
-theta_rad = 10e-6 # Beam divergence angle (radians)
+theta_rad = 5e-6 # Beam divergence angle (radians)
 v_wind = 21 # Wind speed (m/s) for Cn2 profile
 mu_x = 0 # Mean pointing error in x (m)
 mu_y = 0 # Mean pointing error in y (m)
-sigma_theta_x = theta_rad/8 # Standard deviation of pointing error in x (radians)
-sigma_theta_y = theta_rad/8 # Standard deviation of pointing error in y (radians)
+sigma_theta_x = theta_rad/10 # Standard deviation of pointing error in x (radians)
+sigma_theta_y = theta_rad/10 # Standard deviation of pointing error in y (radians)
 e_0 = 0.5 # Detector efficiency
 p_dark = 1e-5 # Dark count probability
 e_pol = 1/100 # Polarization misalignment error
